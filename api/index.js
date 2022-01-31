157cbf6ab9c6a0bd671af29095d85268a0ff133c
Rob Jewell <rob.jewell@ibotta.com>
2022-01-31 11:13:31 -0500

    WIP: API server

--- api/index.js
+++ api/index.js
@@ -17,12 +17,14 @@ app.get('/health', (_req, res) => {
 app.use(users)
 app.use(test)
 
-// Start standalone server if directly running
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`API server listening on port ${port}`)
-})
-
 // Export express app
 module.exports = app
 
+// Start standalone server if directly running
+if (require.main === module) {
+  const port = 3000
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`API server listening on port ${port}`)
+  })
+}
